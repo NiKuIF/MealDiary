0045bbc6ad04ea67606cfecc8d118f8e7ce1a580
admin <admin@admins-MBP.Asus-Router>
2016-08-20 17:10:45 +0200

    added some table view code, but crashes after start in app delegate for no reasson so far...

---
+++ CustomCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomCell.swift
+//  MealDiary
+//
+//  Created by admin on 20/08/16.
+//  Copyright © 2016 Stefan Papst. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MealDiary/ViewController.swift
+++ MealDiary/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    var names = ["stefan", "julia"]
+    var images = [UIImage(named: "stefan"), UIImage(named: "julia")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CustomCell
+        
+        cell.name.text = names[indexPath.row]
+        cell.photo.image = images[indexPath.row]
+     
+        return cell
+        
     }
 
 
